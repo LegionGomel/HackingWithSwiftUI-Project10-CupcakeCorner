2da5cce973b9f94d9109ba3acc41ac2a71e0abe2
Yuri Ershov <shtorm0606@gmail.com>
2022-01-16 16:03:28 +0300

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Yuri Ershov on 16.01.22.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
