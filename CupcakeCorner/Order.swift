341440550b258f187ea51ae53aedd1b263ecc14e
Yuri Ershov <shtorm0606@gmail.com>
2022-01-18 18:18:02 +0300

    Implement initial UI, sample views for screens, and ObservableObject class

---
+++ CupcakeCorner/Order.swift
@@ -0,0 +1,26 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Yuri Ershov on 16.01.22.
+//
+
+import SwiftUI
+
+class Order: ObservableObject {
+    static let types = ["Vanilla", "Strawberry", "Chockolate", "Rainbow"]
+    
+    @Published var type = 0
+    @Published var quantity = 3
+    
+    @Published var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    @Published var extraFrosting = false
+    @Published var addSprinkles = false
+}
