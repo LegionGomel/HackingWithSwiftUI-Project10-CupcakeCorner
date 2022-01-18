1b03e8085be48661031ff36f6518c13d909fa8fd
Yuri Ershov <shtorm0606@gmail.com>
2022-01-18 19:39:19 +0300

    add async task to send order as an encoded JSON

--- CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CheckoutView.swift
@@ -24,13 +24,41 @@ struct CheckoutView: View {
                 Text("Your total is \(order.cost, format: .currency(code: "USD"))")
                     .font(.title)
                 
-                Button("Place Order", action: { })
+                Button("Place Order"){
+                    Task {
+                        await placeOrder()
+                    }
+                }
                     .padding()
             }
         }
         .navigationTitle("Check out")
         .navigationBarTitleDisplayMode(.inline)
     }
+    // asyncronous function to send order object as a JSON in background
+    func placeOrder() async {
+        // try to encode order object
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        // create URLRequest with url string
+        var request = URLRequest(url: url)
+        // set value type to json
+        request.setValue("application/json", forHTTPHeaderField: "Content-type")
+        // configure request type to send data
+        request.httpMethod = "POST"
+        
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            // handle the result
+        } catch {
+            print("Checkout failed")
+        }
+    }
+    
 }
 
 struct CheckoutView_Previews: PreviewProvider {
